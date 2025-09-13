6382f1ae8c8596541cebab55572711fae1eca4b3
Denis Oliver Kropp <Leichenbegatter@outlook.com>
2025-09-13 21:11:13 +0200

    update

--- setup.py
+++ setup.py
@@ -6,9 +6,14 @@ setup(
     py_modules=["runtime"],
     packages=find_packages(),
     install_requires=[
-        "google-genai",
-        "python-dotenv",
-        "pyyaml",
+        "google-genai==1.26.0",
+        "python-dotenv==1.0.0",
+        "pyyaml==6.0.2",
+        "ollama==0.5.3",
+        "colorama==0.4.6",
+        "huggingface_hub==0.34.4",
+        "openai==1.107.0",
+        "mistralai==1.9.10",
     ],
     entry_points={
         "console_scripts": [
