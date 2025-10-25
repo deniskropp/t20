1b85925fea54dadf3ea24f5a5c738f18bf1a13f0
Denis Oliver Kropp <dok@directfb1.org>
2025-10-25 23:16:01 +0200

    cleanup

--- logs/session_87030952-fe54-4441-907a-c6d26a6928c2/gen/files/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript for Project Plan Visualization
-
-// Placeholder for dynamic functionality if needed later
-console.log("Project plan script loaded.");
-
-// Example: Function to highlight a step (not implemented in HTML structure)
-function highlightStep(stepNumber) {
-    console.log("Highlighting step:", stepNumber);
-}
--- new-logs/session_4f35fc2a-c4ac-4f5b-8caf-65b61f3c230e/script.js
+++
@@ -1,2 +0,0 @@
-console.log('Higher Self MTC-LCP scripts loaded.');
-// Future JavaScript functionality will be added here.
